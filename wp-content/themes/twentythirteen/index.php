aebd44d96b53aa769463c940222e86bf54e4b8f2
amilcar zambrano <amilcarzg@gmail.com>
2014-02-22 14:19:37 -0430

    sd

--- wp-content/themes/twentythirteen/index.php
+++ wp-content/themes/twentythirteen/index.php
@@ -19,7 +19,7 @@ get_header(); ?>
 	<div id="primary" class="content-area"> asdasd
 		<div id="content" class="site-content" role="main">
 		<?php if ( have_posts() ) : ?>
-			<div>dddasasdasddasds</div>
+			<div>asdsadsd</div>
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
